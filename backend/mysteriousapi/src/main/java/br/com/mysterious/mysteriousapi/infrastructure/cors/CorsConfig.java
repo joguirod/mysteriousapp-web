1e07da5ebdb89b897298c5793ec4ec0c427af547
guilherme <joguiads@gmail.com>
2025-01-04 12:37:47 -0300

    feat: add cors configuration

---
+++ backend/mysteriousapi/src/main/java/br/com/mysterious/mysteriousapi/infrastructure/cors/CorsConfig.java
@@ -0,0 +1,13 @@
+package br.com.mysterious.mysteriousapi.infrastructure.cors;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:42    00")
+                .allowedMethods("GET", "POST");
+    }
+}
