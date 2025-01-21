189a8279cde8c6bf4caac1e9173eeb7bc1721562
guilherme <joguiads@gmail.com>
2025-01-21 10:43:48 -0300

    feat: adiciona get por status do pedido

--- backend/mysteriousapi/src/main/java/br/com/mysterious/mysteriousapi/presentation/controllers/OrderController.java
+++ backend/mysteriousapi/src/main/java/br/com/mysterious/mysteriousapi/presentation/controllers/OrderController.java
@@ -5,15 +5,14 @@ import br.com.mysterious.mysteriousapi.application.exceptions.MysteriousUserNotF
 import br.com.mysterious.mysteriousapi.application.exceptions.OrderNotFoundException;
 import br.com.mysterious.mysteriousapi.application.exceptions.OrderWithoutProductsException;
 import br.com.mysterious.mysteriousapi.application.usecases.mysteriousUser.CancelOrderUseCase;
-import br.com.mysterious.mysteriousapi.application.usecases.order.CreateOrderUseCase;
-import br.com.mysterious.mysteriousapi.application.usecases.order.GetAllOrdersUseCase;
-import br.com.mysterious.mysteriousapi.application.usecases.order.GetOrderByIdUseCase;
-import br.com.mysterious.mysteriousapi.application.usecases.order.GetOrdersByCustomerIdUseCase;
+import br.com.mysterious.mysteriousapi.application.usecases.order.*;
 import br.com.mysterious.mysteriousapi.domain.entities.order.MysteriousOrder;
+import br.com.mysterious.mysteriousapi.domain.entities.order.OrderStatus;
 import br.com.mysterious.mysteriousapi.presentation.dtos.mappers.MysteriousOrderDTOMapper;
 import br.com.mysterious.mysteriousapi.presentation.dtos.request.ChangeOrderStatusRequestDTO;
 import br.com.mysterious.mysteriousapi.presentation.dtos.request.OrderRequestDTO;
 import br.com.mysterious.mysteriousapi.presentation.dtos.response.OrderResponseDTO;
+import jakarta.persistence.criteria.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,14 +30,16 @@ public class OrderController {
     private final GetOrderByIdUseCase getOrderByIdUseCase;
     private final GetOrdersByCustomerIdUseCase getOrdersByCustomerIdUseCase;
     private final CancelOrderUseCase cancelOrderUseCase;
+    private final GetOrdersByOrderStatusUseCase getOrdersByOrderStatusUseCase;
 
-    public OrderController(MysteriousOrderDTOMapper mysteriousOrderDTOMapper, CreateOrderUseCase createOrderUseCase, GetAllOrdersUseCase getAllOrdersUseCase, GetOrderByIdUseCase getOrderByIdUseCase, GetOrdersByCustomerIdUseCase getOrdersByCustomerIdUseCase, CancelOrderUseCase cancelOrderUseCase) {
+    public OrderController(MysteriousOrderDTOMapper mysteriousOrderDTOMapper, CreateOrderUseCase createOrderUseCase, GetAllOrdersUseCase getAllOrdersUseCase, GetOrderByIdUseCase getOrderByIdUseCase, GetOrdersByCustomerIdUseCase getOrdersByCustomerIdUseCase, CancelOrderUseCase cancelOrderUseCase, GetOrdersByOrderStatusUseCase getOrdersByOrderStatusUseCase) {
         this.mysteriousOrderDTOMapper = mysteriousOrderDTOMapper;
         this.createOrderUseCase = createOrderUseCase;
         this.getAllOrdersUseCase = getAllOrdersUseCase;
         this.getOrderByIdUseCase = getOrderByIdUseCase;
         this.getOrdersByCustomerIdUseCase = getOrdersByCustomerIdUseCase;
         this.cancelOrderUseCase = cancelOrderUseCase;
+        this.getOrdersByOrderStatusUseCase = getOrdersByOrderStatusUseCase;
     }
 
     @PostMapping
@@ -76,5 +77,12 @@ public class OrderController {
     public ResponseEntity<OrderResponseDTO> cancelOrder(@RequestBody ChangeOrderStatusRequestDTO changeOrderStatusRequestDTO) throws OrderNotFoundException, MysteriousUserNotFoundException {
         MysteriousOrder order = cancelOrderUseCase.execute(changeOrderStatusRequestDTO.userId(), changeOrderStatusRequestDTO.orderId());
         return new ResponseEntity<>(mysteriousOrderDTOMapper.toResponseDTO(order), HttpStatus.OK);
+    }
+    
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<OrderResponseDTO>> getOrdersByStatus(@PathVariable int status) {
+        OrderStatus orderStatus = OrderStatus.values()[status];
+        List<MysteriousOrder> orders = getOrdersByOrderStatusUseCase.execute(orderStatus);
+        return new ResponseEntity<>(mysteriousOrderDTOMapper.toResponseDTOList(orders), HttpStatus.OK);
     }
 }
